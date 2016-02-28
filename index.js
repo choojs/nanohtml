229a3bd8c2941a8eae282d74263aa2384737aa23
Kyle Robinson Young <kyle@dontkry.com>
2016-02-28 15:24:32 -0800

    Create elements directly instead of through hyperscript

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
 /* global CustomEvent */
 
-var h = require('hyperscript')
 var hyperx = require('hyperx')
-var hx = hyperx(h)
 var diff = require('diffhtml')
 
-module.exports = function bel () {
+var hx = hyperx(function createElement (tag, props, children) {
+  var el = document.createElement(tag)
+  for (var p in props) {
+    if (props.hasOwnProperty(p)) {
+      el[p] = props[p]
+    }
+  }
+  function appendChild (childs) {
+    if (!Array.isArray(childs)) return
+    for (var i = 0; i < childs.length; i++) {
+      var node = childs[i]
+      if (Array.isArray(node)) {
+        appendChild(node)
+        continue
+      }
+
+      // TODO: Escaping?
+
+      if (typeof node === 'number' ||
+        typeof node === 'boolean' ||
+        node instanceof Date ||
+        node instanceof RegExp) {
+        node = node.toString()
+      }
+
+      if (typeof node === 'string') {
+        node = document.createTextNode(node)
+      }
+
+      if (node && node.nodeName && node.nodeType) {
+        el.appendChild(node)
+      }
+    }
+  }
+  appendChild(children)
+
+  // TODO: Validation checks
+  // TODO: Check for a11y things
+
+  return el
+})
+
+module.exports = function bel (str) {
   var el = hx.apply(this, arguments)
   el.toString = function () {
     return el.outerHTML
