2a8e75d666d41a4a1810d4a5a4e9732bd766ed39
Kyle Robinson Young <kyle@dontkry.com>
2016-03-05 15:56:09 -0800

    Dont use identifiers and let morphdom diff properly
    We lose the context of a child element when it is diffed but can
    querySelector in our element when needed.

--- index.js
+++ index.js
@@ -1,57 +1,16 @@
-var document = require('global/document')
 var hyperx = require('hyperx')
 var morphdom = require('morphdom')
 var create = require('./create.js')
-
-var KEY = 'bel'
 var hx = hyperx(create())
-var id = 0
-var belid
 
 module.exports.create = create
 module.exports = function bel () {
   var el = hx.apply(this, arguments)
-  if (!belid(el)) {
-    belid(el, id)
-    id += 1
-  }
   el.update = function (newel) {
     if (typeof newel === 'function') {
       newel = newel()
     }
-    belid(newel, belid(el))
-    if (el && !el.parentNode) {
-      // Lost element, find it
-      el = document.querySelector('[data-bel="' + el.dataset.bel + '"]')
-    }
-    return morphdom(el, newel, {
-      getNodeKey: function (el) {
-        var id = belid(el)
-        return id || el.id
-      }
-    })
+    return morphdom(el, newel)
   }
   return el
-}
-
-if (typeof document !== 'undefined' && document.head && document.head.dataset) {
-  belid = function belid (el, val) {
-    if (el && el.dataset) {
-      if (arguments.length > 1) {
-        el.dataset[KEY] = val
-      } else {
-        return el.dataset[KEY]
-      }
-    }
-  }
-} else {
-  belid = function belid (el, val) {
-    if (el && typeof el.getAttribute === 'function') {
-      if (arguments.length > 1) {
-        return el.setAttribute('data-' + KEY, val)
-      } else {
-        return el.getAttribute('data-' + KEY)
-      }
-    }
-  }
 }
