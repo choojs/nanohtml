82d9652f5bea7e71c9eff2d29af01e78db4f7506
Kyle Robinson Young <kyle@dontkry.com>
2016-07-05 20:04:56 -0700

    Add test to ensure nested existing node onloads fire correctly

--- test/onload.js
+++ test/onload.js
@@ -1,6 +1,7 @@
 var test = require('tape')
 var bel = require('../')
 var document = require('global/document')
+var morphdom = require('morphdom')
 
 test('fire onload and unload events', function (t) {
   t.plan(2)
@@ -15,4 +16,32 @@ test('fire onload and unload events', function (t) {
   </div>`
   document.body.appendChild(result)
   document.body.removeChild(result)
+})
+
+test('onload with nested existing elements', function (t) {
+  t.plan(3)
+  function render1 () {
+    return bel`<ul onload=${function () {
+      t.ok(true, 'parent fired onload')
+    }}><li>hi</li></ul>`
+  }
+  function render2 () {
+    return bel`<ul><li onload=${function (el) {
+      t.ok(true, 'nested fired onload')
+    }} onunload=${function (el) {
+      t.ok(true, 'nested fired onunload')
+      t.end()
+    }}>hi</li></ul>`
+  }
+  function render3 () {
+    return bel`<ul><li>hi</li></ul>`
+  }
+  var root = render1()
+  document.body.appendChild(root)
+  setTimeout(function () {
+    morphdom(root, render2())
+    setTimeout(function () {
+      morphdom(root, render3())
+    }, 10)
+  }, 10)
 })
