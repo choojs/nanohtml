0797eb1f12e9ff684444542ba492c7c2dc880bb7
Yoshua Wuyts <yoshuawuyts@gmail.com>
2017-05-19 13:24:13 +0200

    fixup! pull in pelo code

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
-module.exports = require('pelo')
+// See https://github.com/shuhei/pelo/issues/5
+
+var Module = require('module')
+
+function handleValue (value) {
+  if (Array.isArray(value)) {
+    // Suppose that each item is a result of html``.
+    return value.join('')
+  }
+  // Ignore event handlers.
+  //     onclick=${(e) => doSomething(e)}
+  // will become
+  //     onclick=""
+  if (typeof value === 'function') {
+    return '""'
+  }
+  if (value === null || value === undefined) {
+    return ''
+  }
+  if (value.__encoded) {
+    return value
+  }
+  var str = value.toString()
+  return str
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;')
+}
+
+function stringify () {
+  var pieces = arguments[0]
+  var output = ''
+  for (var i = 0; i < pieces.length; i++) {
+    output += pieces[i]
+    if (i < pieces.length - 1) {
+      output += handleValue(arguments[i + 1])
+    }
+  }
+  // HACK: Avoid double encoding by marking encoded string
+  // You cannot add properties to string literals
+  // eslint-disable-next-line no-new-wrappers
+  var wrapper = new String(output)
+  wrapper.__encoded = true
+  return wrapper
+}
+
+function replace (moduleId) {
+  var originalRequire = Module.prototype.require
+  Module.prototype.require = function (id) {
+    if (id === moduleId) {
+      return stringify
+    } else {
+      return originalRequire.apply(this, arguments)
+    }
+  }
+}
+stringify.replace = replace
+
+module.exports = stringify
