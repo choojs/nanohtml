664af5b68301291718f2e3d086c6a569e69bf041
jondashkyle <contact@jon-kyle.com>
2017-09-03 13:04:51 -0400

    tests

--- test/raw.js
+++ test/raw.js
@@ -1,11 +1,12 @@
 var test = require('tape')
+var bel = require('../')
 var raw = require('../raw')
 
 test('escape html', function (t) {
   t.plan(1)
 
-  var expected = '<span>Hello&nbsp;there</span>'
-  var result = raw('<span>Hello&nbsp;there</span>')
+  var expected = bel`<span>Hello there</span>`.toString()
+  var result = raw('<span>Hello&nbsp;there</span>').toString()
 
   t.equal(expected, result)
   t.end()
