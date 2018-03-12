8d23a8ea8fd081915605767b3a869b61ee60072d
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-03-12 15:00:55 +0100

    fix type annotation

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,4 @@
-declare module "bel" {
+declare module "nanohtml" {
   export default function (strings: TemplateStringsArray, ...keys: any[]): HTMLElement;
   export function createElement (tag: string, attributes: any, children: Array<any>): HTMLElement;
 }
