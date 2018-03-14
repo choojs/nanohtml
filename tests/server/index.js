977299aeff6fda995fa70d49b8e6699aae466ed6
Renée Kooi <renee@kooi.me>
2018-03-14 13:27:26 +0100

    fix empty values, event handlers with or without quotes

--- tests/server/index.js
+++ tests/server/index.js
@@ -39,7 +39,7 @@ test('unescape html', function (t) {
   var expected = '<span>Hello <strong>there</strong></span>'
   var result = raw('<span>Hello <strong>there</strong></span>').toString()
 
-  t.equal(expected, result)
+  t.equal(result, expected)
   t.end()
 })
 
@@ -49,19 +49,17 @@ test('unescape html inside html', function (t) {
   var expected = '<span>Hello <strong>there</strong></span>'
   var result = html`${raw('<span>Hello <strong>there</strong></span>')}`.toString()
 
-  t.equal(expected, result)
+  t.equal(result, expected)
   t.end()
 })
 
 test('event attribute', function (t) {
   t.plan(1)
 
-  var expected = '<div>Hello</div>'
-  var result = html`
-    <div onmouseover=${onmouseover} onmouseout="${onmouseout}">Hello</div>
-  `
+  var expected = '<div onmouseover="" onmouseout="">Hello</div>'
+  var result = html`<div onmouseover=${onmouseover} onmouseout="${onmouseout}">Hello</div>`.toString()
 
-  t.equal(expected, result)
+  t.equal(result, expected)
   t.end()
 
   function onmouseover () {}
@@ -72,11 +70,9 @@ test('boolean attribute', function (t) {
   t.plan(1)
 
   var expected = '<input disabled="disabled">'
-  var result = html`
-    <input disabled=${true} autofocus=${false}>
-  `
+  var result = html`<input disabled=${true} autofocus=${false}>`.toString()
 
-  t.equal(expected, result)
+  t.equal(result, expected)
   t.end()
 })
 
@@ -85,10 +81,8 @@ test('spread attributes', function (t) {
 
   var expected = '<div class="abc" id="def">Hello</div>'
   var props = { class: 'abc', id: 'def' }
-  var result = html`
-    <div ${props}>Hello</div>
-  `
+  var result = html`<div ${props}>Hello</div>`.toString()
 
-  t.equal(expected, result)
+  t.equal(result, expected)
   t.end()
 })
