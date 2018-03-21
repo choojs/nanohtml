43df5e9a3a5cd07ed58e1aeb4d93fd1427fd10df
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-03-21 13:47:46 +0100

    moved raw codes for browser and server

--- raw.js
+++ raw.js
@@ -1,19 +1,3 @@
 module.exports = typeof window !== 'undefined'
-  ? nanohtmlRawBrowser
-  : nanohtmlRawServer
-
-function nanohtmlRawBrowser (tag) {
-  var el = document.createElement('div')
-  el.innerHTML = tag
-  return toArray(el.childNodes)
-}
-
-function nanohtmlRawServer (tag) {
-  var wrapper = new String(tag) // eslint-disable-line no-new-wrappers
-  wrapper.__encoded = true
-  return wrapper
-}
-
-function toArray (arr) {
-  return Array.isArray(arr) ? arr : [].slice.call(arr)
-}
+  ? require('./lib/raw-browser')
+  : require('./lib/raw-server')
