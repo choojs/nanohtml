a79e0241dcb0e08d1f2951750eb5ffa82a14584e
Jacob Evelyn <jacobevelyn@gmail.com>
2019-10-24 12:43:57 -0400

    Add nanohtml/raw to index.d.ts (#159)

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,3 +2,7 @@ declare module "nanohtml" {
   export default function (strings: TemplateStringsArray, ...keys: any[]): HTMLElement;
   export function createElement (tag: string, attributes: any, children: Array<any>): HTMLElement;
 }
+
+declare module "nanohtml/raw" {
+  export default function raw(tag: string): ChildNode[] | string;
+}
