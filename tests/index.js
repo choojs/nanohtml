bcbcb9099d77168ed667cf5955f808f092e7664c
Kagami Sascha Rosylight <saschanaz@outlook.com>
2019-10-25 02:28:21 +0900

    node version check

--- tests/index.js
+++ tests/index.js
@@ -1,4 +1,11 @@
-require('./browser')
+function getNodeMajor () {
+  return process.version.split('.')[0].slice(1)
+}
+
+if (typeof process === 'undefined' || getNodeMajor() >= 8) {
+  require('./browser')
+}
+
 if (typeof window === 'undefined') {
   require('./server')
   require('./transform')
