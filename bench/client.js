e8a93da3c139ef5d7d9a34cd2c2519a63e266d23
Carl Törnqvist <carl@tornqv.ist>
2020-02-06 10:21:16 +0100

    Add bench comparison

--- bench/client.js
+++ bench/client.js
@@ -1,11 +1,25 @@
 var nanobench = require('nanobench')
 
 var render = require('../render')
-var nanoHtml = require('../html')
+var nanoHtml = require('../') // old
+var nanoHtmlNext = require('../html') // new
+var morph = require('nanomorph')
 var createApp = require('./fixtures/app')
 
 nanobench('nanohtml browser 10000 iterations', function (b) {
   var app = createApp(nanoHtml)
+  var div = document.createElement('div')
+
+  document.body.appendChild(div)
+  for (var i = 0; i < 100; i++) morph(div, app.render())
+
+  b.start()
+  for (i = 0; i < 10000; i++) morph(div, app.render())
+  b.end()
+})
+
+nanobench('nanohtml (next) browser 10000 iterations', function (b) {
+  var app = createApp(nanoHtmlNext)
   var div = document.createElement('div')
 
   document.body.appendChild(div)
